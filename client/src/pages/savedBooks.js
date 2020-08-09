75daf5fa3760dd4da7fd1d317f6a970995ed8f28
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-08-09 06:32:20 -0400

    nav added

--- client/src/pages/savedBooks.js
+++ client/src/pages/savedBooks.js
@@ -8,22 +8,47 @@ class savedBooks extends Component {
   };
 
   componentDidMount() {
-    this.loadBooks();
-  }
-
-  loadBooks() {
     API.getAllBooks()
-      .then((res) => {
-        const books = res.data;
-        console.log(books);
-        this.setState({ svBooks: books });
-      })
-      .catch((err) => console.log(err));
-  }
+    .then((res) => {
+      const books = res.data;
+      console.log(books);
+      this.setState({ svBooks: books });
+    })
+    .catch((err) => console.log(err));  }
+
+  // loadBooks() {
+    
+  // }
   render() {
     return (
       <div>
-        <DisplaySavedBooks svBooks={this.state.svBooks} />
+        {/* <DisplaySavedBooks svBooks={this.state.svBooks} /> */}
+        <h1>Saved Books are displayed here</h1>
+      {this.state.books.map((book) => {
+        return(
+          <div key={book.id}>
+            <ul>
+              <li>title:{book.title}</li>
+              <li>author:{book.authors}</li>
+              <li>descr:{book.description}</li>
+              <li>
+                image:
+                <img
+                  src={book.thumbnail}
+                  alt={book.title}
+                />
+              </li>
+              <li>
+                Whant more details about the book?
+                <a href={book.infoLink}> Click here</a>
+              </li>
+              <li>
+                =================================================================
+              </li>
+            </ul>
+          </div>
+       )
+      })}
       </div>
     );
   }
